2cfe067cfad440f87b8f3f9c0dd6127a6a5bea1d
Matheus C. Xavier <107576931+mthxavier@users.noreply.github.com>
2026-05-13 16:15:59 -0300

    fix: atributo novo do PDO

--- backend/config/database.php
+++ backend/config/database.php
@@ -1,4 +1,9 @@
 <?php
+
+$mysqlSslCaAttribute = defined(\Pdo\Mysql::class . '::ATTR_SSL_CA')
+    ? \Pdo\Mysql::ATTR_SSL_CA
+    : PDO::MYSQL_ATTR_SSL_CA;
+
 return [
     'default' => env('DB_CONNECTION', 'sqlite'),
     'connections' => [
@@ -18,7 +23,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                $mysqlSslCaAttribute => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
         'sqlite' => [
